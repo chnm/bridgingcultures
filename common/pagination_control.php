a06af59267f61dc78d135a74f8558e242acaeb17
Kim Nguyen <design@kimisgold>
2012-06-13 15:15:56 -0400

    Custom pagination markup.

---
+++ common/pagination_control.php
@@ -0,0 +1,29 @@
+<?php if ($this->pageCount > 1): ?>
+<ul class="pagination_list">
+
+    <li><?php echo __('Page ') . $this->current . __(' of ') . $this->last; ?></li>
+    
+    <?php if (isset($this->previous)): ?>
+    <!-- Previous page link --> 
+        <?php if(isset($this->next)): ?>
+        <li class="pagination_previous both">
+        <?php else: ?>
+        <li class="pagination_previous">
+        <?php endif; ?>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->previous), null, $_GET)); ?>"><?php echo __('Previous'); ?></a>
+    </li>
+    <?php endif; ?>
+        
+    <?php if (isset($this->next)): ?> 
+    <!-- Next page link -->
+        <?php if(isset($this->previous)): ?>
+        <li class="pagination_next both">
+        <?php else: ?>
+        <li class="pagination_next">
+        <?php endif; ?>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->next), null, $_GET)); ?>"><?php echo __('Next'); ?></a>
+    </li>
+    <?php endif; ?>
+
+</ul>
+<?php endif; ?>
