6f00cd9910543d191eba7bd6f40663300d9b06f9
{kimisgold} <{design@kimisgold.com}>
2014-06-11 13:34:22 -0400

    Implement item name sorting helper and update indentation.

--- custom.php
+++ custom.php
@@ -1,87 +1,87 @@
 <?php 
 
 function bc_display_filters() {
-	$request = Zend_Controller_Front::getInstance()->getRequest(); 
-	$requestArray = $request->getParams();
-	$db = get_db();
-	$displayArray = array();
-	foreach ($requestArray as $key => $value) {
-	    $filter = $key;
-	    if($value != null) {
-	    	$displayValue = null;
-	    	switch ($key) {	    		
-	    		case 'type':
-	    			$filter = 'Item Type';
-	    			$itemtype = $db->getTable('ItemType')->find($value);
-	    			$displayValue = $itemtype->name;
-	    			break;
-	    		
-	    	 	case 'collection':					
-	    	    	$collection = $db->getTable('Collection')->find($value);
-	    	    	$displayValue = $collection->name;
-	    	    	break;
+    $request = Zend_Controller_Front::getInstance()->getRequest(); 
+    $requestArray = $request->getParams();
+    $db = get_db();
+    $displayArray = array();
+    foreach ($requestArray as $key => $value) {
+        $filter = $key;
+        if($value != null) {
+            $displayValue = null;
+            switch ($key) {                
+                case 'type':
+                    $filter = 'Item Type';
+                    $itemtype = $db->getTable('ItemType')->find($value);
+                    $displayValue = $itemtype->name;
+                    break;
+                
+                 case 'collection':                    
+                    $collection = $db->getTable('Collection')->find($value);
+                    $displayValue = $collection->name;
+                    break;
 
-	    		case 'user':
-	    	    	$user = $db->getTable('User')->find($value);
-	    	    	$displayValue = $user->Entity->getName();
-	    	    	break;
+                case 'user':
+                    $user = $db->getTable('User')->find($value);
+                    $displayValue = $user->Entity->getName();
+                    break;
 
-	    	    case 'public':
-	    	    case 'featured':
-	    	    	if($value==1) { 
-	    	    		$displayValue = 'yes';
-	    	    	} else {
-	    	    		$displayValue = 'no';
-	    	    	}
-	    	    	break;
-	    	    	
-	    	    case 'search':
-	    	    case 'tags':
-	    	    case 'range':
-	    	    	$displayValue = $value;
-	    	    	break;
-	    	}
-			if ($displayValue) {
-				$displayArray[$filter] = $displayValue;
-			}
-	    }
-	}
-  	if(array_key_exists('advanced', $requestArray)) {
-	    $advancedArray = array();
-	    foreach ($requestArray['advanced'] as $i => $row) {
-	        if (!$row['element_id'] || !$row['type']) {
-	        	continue;
-	        }
-	        $elementID = $row['element_id'];
-	        $elementDb = $db->getTable('Element')->find($elementID);
-	        $element = $elementDb->name;
-	        $type = $row['type'];
-	        $terms = $row['terms'];
-	        $advancedValue = $element . ' ' . $type;
-	        if ($terms) {
-	        	$advancedValue .= ' "' . $terms . '"';
-	        }
-	    	$advancedArray[$i] = $advancedValue; 
-	    	/* Advanced needs a separate array from $displayValue because it's 
-	    	   possible for "Specific Fields" to have multiple values due to 
-	    	   the ability to add fields. */
-	    }
-	} 
-	if (!empty($displayArray) || !empty($advancedArray)) {
-		$html = '<div class="filters">';
-		$html .= '<ul id="filter-list">';
-		foreach($displayArray as $name => $query) {
-			$html .= '<li id="' . $name . '">' . ucfirst($name) . ': ' . $query . '</li>';
-		}
-		if(!empty($advancedArray)) {
-			foreach($advancedArray as $j => $advanced) {
-				$html .= '<li id="advanced">' . $advanced . '</li>';
-			}
-		}
-		$html .= '</ul>';
-		$html .= '</div>';
-		echo $html;
-	}
+                case 'public':
+                case 'featured':
+                    if($value==1) { 
+                        $displayValue = 'yes';
+                    } else {
+                        $displayValue = 'no';
+                    }
+                    break;
+                    
+                case 'search':
+                case 'tags':
+                case 'range':
+                    $displayValue = $value;
+                    break;
+            }
+            if ($displayValue) {
+                $displayArray[$filter] = $displayValue;
+            }
+        }
+    }
+      if(array_key_exists('advanced', $requestArray)) {
+        $advancedArray = array();
+        foreach ($requestArray['advanced'] as $i => $row) {
+            if (!$row['element_id'] || !$row['type']) {
+                continue;
+            }
+            $elementID = $row['element_id'];
+            $elementDb = $db->getTable('Element')->find($elementID);
+            $element = $elementDb->name;
+            $type = $row['type'];
+            $terms = $row['terms'];
+            $advancedValue = $element . ' ' . $type;
+            if ($terms) {
+                $advancedValue .= ' "' . $terms . '"';
+            }
+            $advancedArray[$i] = $advancedValue; 
+            /* Advanced needs a separate array from $displayValue because it's 
+               possible for "Specific Fields" to have multiple values due to 
+               the ability to add fields. */
+        }
+    } 
+    if (!empty($displayArray) || !empty($advancedArray)) {
+        $html = '<div class="filters">';
+        $html .= '<ul id="filter-list">';
+        foreach($displayArray as $name => $query) {
+            $html .= '<li id="' . $name . '">' . ucfirst($name) . ': ' . $query . '</li>';
+        }
+        if(!empty($advancedArray)) {
+            foreach($advancedArray as $j => $advanced) {
+                $html .= '<li id="advanced">' . $advanced . '</li>';
+            }
+        }
+        $html .= '</ul>';
+        $html .= '</div>';
+        echo $html;
+    }
 }
 
 function bc_link_to_items_with_element_text($text = null, $props = array(), $action = 'browse', $elementId = null, $elementText = null) {
@@ -130,5 +130,28 @@ function bc_simple_search($buttonText = null, $formProperties=array('class'=>'si
     return $html;
 }
 
+function bc_compare_related_items($firstItem, $secondItem) {
+    $firstItemName = item('Dublin Core', 'Title', array(), $firstItem);
+    $secondItemName = item('Dublin Core', 'Title', array(), $secondItem);
+    if ($firstItemName == $secondItemName ) { return 0 ; } 
+    return ($firstItemName < $secondItemName) ? -1 : 1;
+}
+
+function bc_link_to_item($item = null) {
+    if (!$item) {
+        $item = get_current_item();
+    }
+    if (count(item('Dublin Core', 'Title', array('all' => true), $item)) > 1): 
+        echo link_to_item(
+            item('Dublin Core', 'Title', array('index' => 1), $item), 
+            array('class'=>'permalink')
+        ); 
+    else: 
+        echo link_to_item(
+            item('Dublin Core', 'Title', array(), $item),                     
+            array('class'=>'permalink')
+        ); 
+    endif;                     
+}
 
 ?>
