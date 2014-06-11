fdb6d005fa26b084b463d9d625e8e0fd9d14bd35
{kimisgold} <{design@kimisgold.com}>
2014-06-11 13:32:00 -0400

    Add lightbox and paging js and view for news/feature post type, use includes for news post views, apply string changes, set properties for lightbox, add embed codes and various description checks.

--- items/show.php
+++ items/show.php
@@ -1,219 +1,122 @@
-<?php head(array('title' => item('Dublin Core', 'Title'),'bodyid'=>'item','bodyclass' => 'show item')); 
-$currentItemId = item('id');
+<?php queue_js(array('quickpager.jquery','orangebox.min')); ?>
+
+<?php head(array(
+        'title' => item('Dublin Core', 'Title'),
+        'bodyid'=> (item_has_type('News/Feature Post') ? 'simple-page' : 'item'),
+        'bodyclass' => 'show item')
+      ); 
+
+      $currentItemId = item('id');
 ?>
 
     <?php if(item_has_type('Book')): ?>
 
-        <nav id="page-menu" class="four columns offset-by-one alpha">
-        
-            <?php 
-            $imageFile = get_db()->getTable('File')->findWithImages($item->id, 0);
-            if(($fullsizeHtml = display_file($imageFile, array('imageSize' => 'fullsize', 'linkAttributes' => array('class' => 'lightbox book-image', 'data-ob' => 'lightbox')), array('class' => 'book-image')))):
-                echo $fullsizeHtml;
-            else:
-                echo '<p class="no-image">No book cover.</p>';
-            
-            endif; ?>
-            
-            <ul>
-                <?php if(item('Dublin Core', 'Description')): ?>                
-                    <li><a href="#summary">Book Summary</a></li>
-                <?php endif; ?>
-                <?php if(item('Item Type Metadata', 'Author Biography')): ?>                
-                    <li><a href="#author">About the Author</a></li>
-                <?php endif; ?>
-                    <li><a href="#annotation">Annotation</a></li>
-                <?php if($resources = mj_get_related_to_book_or_essay()): ?>
-                    <li><a href="#resources">Related Resources</a></li>
-                <?php endif; ?>
-            </ul>
-                                
-        </nav>
-        
-        <div id="right-content" class="nine columns">
-        
-            <h3 class="book-theme">
-            <?php if($themes =  multicollections_get_collections_for_item()):
-                $themesCount = count($themes);
-                $i = 0;
-                if($themesCount > 1) {
-                    echo 'Themes: ';
-                } else {
-                    echo 'Theme: ';
-                }
-                foreach($themes as $theme) {
-                    echo link_to_collection($theme->name, array(), 'show', $theme);
-                    $i++;
-                    if($i < $themesCount) {
-                        echo ', ';
-                    }
-                } ?>
-            <?php endif; ?>
-            </h3>
-            <?php if(count(item('Dublin Core', 'Title', array('all' => true))) > 1): ?>
-            <h2 class="book-title"><?php echo item('Dublin Core', 'Title', array('index' => 1)); ?><br>
-            <?php else: ?>
-            <h2 class="book-title"><?php echo item('Dublin Core', 'Title'); ?><br>
-            <?php endif; ?>
-            <span class="book-author">by <?php echo item('Dublin Core', 'Creator'); ?></span></h2>
-            
-            <div class="book summary">
-                
-                <?php if(item('Dublin Core', 'Description')): ?>
-                
-                    <h3><a name="summary"></a>Book Summary</h3>
-                    
-                <?php endif; ?>
-            
-                <?php echo item('Dublin Core', 'Description'); ?>
-                
-                <?php if(item('Item Type Metadata', 'Author Biography')): ?>
-                
-                    <h3><a name="author"></a>About the Author</h3>
-                    
-                    <?php echo item_thumbnail(array('class' => 'author-image'), 1); ?>
-                    
-                    <p><?php echo item('Item Type Metadata', 'Author Biography'); ?></p>
-                    
-                <?php endif; ?>
-                
-                <h3><a name="annotation"></a>Annotation</h3>
-                
-                <?php echo item('Item Type Metadata', 'Text'); ?>
-                
-                <p><strong>Region: </strong>
-                <?php echo item('Item Type Metadata', 'Region', array('delimiter' => ', ')); ?></p>
-                
-                <p><strong>Time Period: </strong>
-                <?php echo item('Item Type Metadata', 'Time Period', array('delimiter' => ', ')); ?></p>
-                                
-                <h3>How to Cite This Source</h3>
-                
-                <p>"Muslim Journeys | Item #<?php echo item('id'); ?>: <?php echo item('Dublin Core', 'Title', array('index' => 1)); ?>", <?php echo date('F d, Y'); ?> <?php echo item('permalink'); ?>.</p>
-                
-                <?php if(item_has_tags()): ?>
-                
-                <h3>Tags</h3>
-                
-                <p><?php echo item_tags_as_string(); ?></p>
-                
-                <?php endif; ?>
-                
-                                
-                <?php if($resources): ?>
-                
-                    <h3><a name="resources"></a>Related Resources</h3>
-                    
-                    <ul>
-                
-                    <?php set_items_for_loop($resources);
-                        
-                    while(loop_items()):
-                    
-                        echo '<li>' . link_to_item(item('Dublin Core', 'Title')) . '</li>';
-                        
-                    endwhile; ?>
-                    
-                    </ul>
-                    
-                <?php endif; ?>
-                
-            </div>
-        
-        </div>
+        <?php include('books.php'); ?>
         
     <?php elseif (item_has_type('Essay')): ?>
     
-        <nav id="page-menu" class="five columns offset-by-one alpha">
+        <?php include('essay.php'); ?>
+
+    <?php elseif (item_has_type('News/Feature Post')): ?>
         
-            <?php 
-            
-            if($imageFile = get_db()->getTable('File')->findWithImages($item->id, 0)):
-                $fullsizeHtml = display_file($imageFile, array('imageSize' => 'fullsize', 'linkAttributes' => array('class' => 'lightbox book-image', 'data-ob' => 'lightbox' )), array('class' => 'book-image'));
-                echo $fullsizeHtml;
-            elseif(item_has_files()):
-                echo display_files_for_item(
-        		    array(
-        		      'linkToFile' => true,
-    		          'linkAttributes' => array('data-ob_iframe' => true, 'data-ob' => 'lightbox')
-        		    )
-        		);
-            else:
-                echo '<p class="no-image">No image.</p>';
-            
-            endif; ?>
-                                                
-        </nav>
-        
-        <div id="right-content" class="eight columns offset-by-one">
-            <h3 class="book-theme">
-            <?php if($themes =  multicollections_get_collections_for_item()):
-                $themesCount = count($themes);
-                $i = 0;
-                if($themesCount > 1) {
-                    echo 'Themes: ';
-                } else {
-                    echo 'Theme: ';
-                }
-                foreach($themes as $theme) {
-                    echo link_to_collection($theme->name, array(), 'show', $theme);
-                    $i++;
-                    if($i < $themesCount) {
-                        echo ', ';
-                    }
-                } ?>
-            <?php endif; ?>
-            
-            <?php
-            set_current_item(get_item_by_id($currentItemId));
-            ?>
-            <h2 class="item-title"><?php echo item('Dublin Core', 'Title'); ?></h2>
-            
-            
-            <div class="paging">
-                        
-            <?php echo item('Dublin Core', 'Description'); ?>
-            
-            </div>
-            
-            
-        </div>
-    
+        <?php include('news.php'); ?>
     
     <?php else: ?>
         
-        <nav id="page-menu" class="five columns offset-by-one alpha">
-        
+        <nav id="page-menu" class="five columns alpha">
+
+            <?php set_current_item(get_item_by_id($currentItemId)); ?>
+            <?php $artspots = false; ?>
+            <?php if($themes =  multicollections_get_collections_for_item()):
+                $themesCount = count($themes);
+                $i = 0;
+                foreach($themes as $theme) {
+                    if($theme->name == 'Art Spots') { $artspots = true; }
+                    $i++;
+                } ?>
+            <?php endif; ?>
+
             <?php 
             
-            $audioTypes = array('audio/aac','audio/aiff','audio/mid','audio/midi','audio/mp3','audio/mp4','audio/mpeg','audio/mpeg3','audio/ogg','audio/wav','audio/wma','audio/x-aac','audio/x-aiff','audio/x-midi','audio/x-mp3','audio/x-mp4','audio/x-mpeg','audio/x-mpeg3','audio/x-mpegaudio','audio/x-ms-wax','audio/x-realaudio','audio/x-wav','audio/x-wma');
-            $videoTypes = array('video/avi','video/divx','video/mp4','video/mpeg','video/msvideo','video/ogg','video/quicktime','video/x-ms-wmv','video/x-msvideo', 'video/m4v');
+            $audioTypes = array(
+                'audio/aac','audio/aiff','audio/mid','audio/midi','audio/mp3',
+                'audio/mp4','audio/mpeg','audio/mpeg3','audio/ogg','audio/wav',
+                'audio/wma','audio/x-aac','audio/x-aiff','audio/x-midi','audio/x-mp3',
+                'audio/x-mp4','audio/x-mpeg','audio/x-mpeg3','audio/x-mpegaudio','audio/x-ms-wax',
+                'audio/x-realaudio','audio/x-wav','audio/x-wma'
+            );
             
-            if($imageFile = get_db()->getTable('File')->findWithImages($item->id, 0)):
-                $fullsizeHtml = display_file($imageFile, array('imageSize' => 'fullsize', 'linkAttributes' => array('class' => 'lightbox book-image', 'data-ob' => 'lightbox' )), array('class' => 'book-image'));
-                echo $fullsizeHtml;
-            elseif(item_has_files()):
+            $videoTypes = array(
+                'video/avi','video/divx','video/mp4','video/mpeg','video/msvideo',
+                'video/ogg','video/quicktime','video/x-ms-wmv','video/x-msvideo', 'video/m4v'
+            );
+
+            if(item_has_files() && $artspots == false):
                 foreach($item->Files as $file) {
+                    $wrapper = false;
                     if(array_search($file->getMimeType(), $videoTypes) !== false) {
                         echo '<a href="#video" data-ob="lightbox" data-ob_width="640" class="video-preview"><img src="' . uri('themes/muslimjourneys/images/audio.jpg') . '"></a>';
-                        echo '<div id="video">';
+                        $wrapper = true;
+                        echo '<div id="video" style="width: 600px; height: 340px;">';
+                        echo display_file( $file, 
+                            array(
+                              'linkToFile' => true,
+                              'linkAttributes' => array('data-ob_iframe' => true, 'data-ob' => 'lightbox'),
+                              'width' => 600,
+                              'height' => 340
+                            )
+                        );
                     } elseif(array_search($file->getMimeType(), $audioTypes) !== false) {
                         echo '<a href="#audio" data-ob="lightbox" data-ob_width="640" class="video-preview"><img src="' . uri('themes/muslimjourneys/images/audio.jpg') . '"></a>';
                         echo '<div id="audio">';
-                    }                         
-                    echo display_files_for_item(
-            		    array(
-            		      'linkToFile' => true,
-        		          'linkAttributes' => array('data-ob_iframe' => true, 'data-ob' => 'lightbox'),
-        		          'width' => 600
-            		    )
-            		);            
-                    echo '</div>';
+                        echo display_file( $file, 
+                            array(
+                              'linkToFile' => true,
+                              'linkAttributes' => array('data-ob_iframe' => true, 'data-ob' => 'lightbox'),
+                            )
+                        );
+                        $wrapper = true;
+                    } elseif($imageFile = get_db()->getTable('File')->findWithImages($item->id, 0)) {
+                            $fullsizeHtml = display_file($imageFile, array('imageSize' => 'fullsize', 'linkAttributes' => array('class' => 'lightbox book-image', 'data-ob' => 'lightbox' )), array('class' => 'book-image'));
+                            echo $fullsizeHtml;
+                            break;
+                    }
+                    if ($wrapper) { 
+                        echo '</div>'; 
+                        break;
+                    }
                 }
+            elseif($embed = item('Item Type Metadata', 'Embed code')):
+                echo '<a href="#video" data-ob="lightbox" data-ob_width="640" class="video-preview"><img src="' . uri('themes/muslimjourneys/images/audio.jpg') . '"></a>';
+                echo '<div id="video" style="width: 640px; height: 364px;">';
+                echo $embed;
+                echo '</div>';
+            elseif($imageFile = get_db()->getTable('File')->findWithImages($item->id, 0)):
+                $fullsizeHtml = display_file($imageFile, array('imageSize' => 'fullsize', 'linkAttributes' => array('class' => 'lightbox book-image', 'data-ob' => 'lightbox' )), array('class' => 'book-image'));
+                echo $fullsizeHtml;
             else:
                 echo '<p class="no-image">No image.</p>';
-            
             endif; ?>
+            
+            <ul>
+
+                <?php if(item('Dublin Core', 'Description')): ?>                
+                <li><a href="#about">About This Resource</a></li>
+                <?php endif; ?>
+
+                <?php if(item('Item Type Metadata', 'Text')): ?>                
+                <li><a href="#text">Text</a></li>
+                <?php endif; ?>
+
+                <?php if(item('Item Type Metadata', 'Transcription')): ?>                
+                <li><a href="#transcription">Transcription</a></li>
+                <?php endif; ?>
+
+                <li><a href="#source">Source</a></li>
+
+                <li><a href="#cite">How to Cite This Page</a></li>
+
+            </ul>
                                                 
             <h4>Related Books</h4>
             
@@ -232,58 +135,57 @@ $currentItemId = item('id');
                 
             
             ?>
-            
 
-        
         </nav>
         
-        <div id="right-content" class="eight columns offset-by-one">
+        <div id="right-content" class="ten columns offset-by-one omega">
             <h3 class="book-theme">
-            <?php if($themes =  multicollections_get_collections_for_item()):
-                $themesCount = count($themes);
+            <?php set_current_item(get_item_by_id($currentItemId)); ?>            
+            <?php $themesCount = count($themes);
                 $i = 0;
-                if($themesCount > 1) {
-                    echo 'Themes: ';
-                } else {
-                    echo 'Theme: ';
-                }
                 foreach($themes as $theme) {
-                    echo link_to_collection($theme->name, array(), 'show', $theme);
+                    echo '<a href="' . uri('themes/show/' . $theme->id) . '">' . $theme->name . '</a>';
                     $i++;
                     if($i < $themesCount) {
                         echo ', ';
                     }
                 } ?>
-            <?php endif; ?>
-            
-            <?php
-            set_current_item(get_item_by_id($currentItemId));
-            ?>            
+            </h3>
+                      
             <h2 class="item-title"><?php echo item('Dublin Core', 'Title'); ?></h2>
             
-            <?php if (!item_has_type('Further Reading')): ?>
-                <h3>Annotation</h3>
+            <?php if (item_has_files()): ?>
+                <?php while(loop_files_for_item()): ?>
+                    <?php if (item_file('MIME Type') == 'application/pdf'): ?>
+                        <p class="discussion"><a href="<?php echo item_file('uri'); ?>" class="theme-pdf">Download PDF of <?php echo item('Dublin Core', 'Title'); ?></a></p>
+                    <?php endif; ?>
+                <?php endwhile; ?>
             <?php endif; ?>
             
-            <?php if (!item_has_type('Further Reading')): ?>
+            <?php if (item('Dublin Core', 'Description')): ?>
+                <a name="about"></a><h3>About This Resource</h3>
                 <p><?php echo item('Dublin Core', 'Description'); ?></p>
-            <?php else: ?>
-                <?php echo item('Item Type Metadata', 'Text'); ?>
             <?php endif; ?>
-            
-            <p><strong>Region:</strong> <?php echo item('Item Type Metadata', 'Region', array('delimiter' => ', ')); ?></p>
-            
-            <p><strong>Time Period:</strong> <?php echo item('Item Type Metadata', 'Time Period', array('delimiter' => ', ')); ?></p>
-            
+
+            <?php if (item('Item Type Metadata', 'Text')): ?>
+                <h3><a name="text">Text</a></h3>
+                <div class="text"><?php echo item('Item Type Metadata', 'Text'); ?></div>
+            <?php endif; ?>
+
+            <?php if (item('Item Type Metadata', 'Transcription')): ?>
+                <h3><a name="transcription">Transcription</a></h3>
+                <?php echo item('Item Type Metadata', 'Transcription'); ?>
+            <?php endif; ?>
+
             <?php if(item('Dublin Core', 'Source')): ?>
 
-                <h3>Source</h3>
+                <a name="source"></a><h3>Source</h3>
                 
                 <p><?php echo item('Dublin Core', 'Source'); ?></p>
                 
             <?php endif; ?>
             
-            <h3>How to Cite This Source</h3>
+            <a name="cite"></a><h3>How to Cite This Page</h3>
             
             <p>"Muslim Journeys | Item #<?php echo item('id'); ?>: <?php echo item('Dublin Core', 'Title'); ?>", <?php echo date('F d, Y'); ?> <?php echo item('permalink'); ?>.</p>
             
@@ -294,7 +196,7 @@ $currentItemId = item('id');
             <p><?php echo item_tags_as_string(); ?></p>
             
             <?php endif; ?>
-        
+            
         </div>
     
     <?php endif; ?>
